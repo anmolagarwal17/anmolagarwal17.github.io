3808b73a5af90177e5c7451a17de4799bb1ef558
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-08-17 09:24:49 +0530

    skills section done & form testing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const about = document.getElementById('about');
 const projects = document.getElementById('projects');
 const skills = document.getElementById('skills');
 const education = document.getElementById('education');
-const interests = document.getElementById('interests');
+// const interests = document.getElementById('interests');
 const contact = document.getElementById('contact');
 const navSec = document.querySelectorAll('#nav-sec a');
 
@@ -23,14 +23,18 @@ const navSec = document.querySelectorAll('#nav-sec a');
 // 	}
 // });
 
-const secHeights = [home.clientHeight, about.clientHeight, projects.clientHeight, skills.clientHeight, education.clientHeight, interests.clientHeight, contact.clientHeight];
+const secHeights = [home.clientHeight, about.clientHeight, projects.clientHeight, skills.clientHeight, education.clientHeight, contact.clientHeight];
 
 // for nav selection animation
 window.addEventListener('scroll', function(){
-	let sum = window.innerHeight/2;
-	let i=0;
-	while(sum < window.scrollY)
-		sum += secHeights[i++];
+	// let sum = window.innerHeight/2;
+	let sum = 2*secHeights[0]/3;
+	let i=1;
+	while(sum < window.scrollY){
+		sum += secHeights[i-1]/3 + 2*secHeights[i]/3;
+		i++;
+	}
+	i--;
 	for (let j = 0; j < navSec.length; j++) {
 		navSec[j].classList.remove('selected');
 	}
