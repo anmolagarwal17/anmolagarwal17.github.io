758edad9d521ff6a5f4394a5ed57613ba12bbcb9
Anmol-Agarwal <a.a.rocks17@gmail.com>
2020-08-18 01:35:32 +0530

    title reveal animations added

--- main.js
+++ main.js
@@ -6,32 +6,25 @@ const education = document.getElementById('education');
 // const interests = document.getElementById('interests');
 const contact = document.getElementById('contact');
 const navSec = document.querySelectorAll('#nav-sec a');
+const secs = [home, about, projects, skills, education, contact];
 
-// window.addEventListener('scroll', function () {
-// 	var two = document.querySelector('#projects');
-// 	if (
-// 		window.scrollY +
-// 			(Math.max(
-// 				document.documentElement.clientHeight,
-// 				window.innerHeight || 0
-// 			) *
-// 				3) /
-// 				5 >
-// 		two.offsetTop + two.offsetHeight
-// 	) {
-// 		console.log('You got it!');
-// 	}
-// });
+const secHeights = [
+	home.clientHeight,
+	about.clientHeight,
+	projects.clientHeight,
+	skills.clientHeight,
+	education.clientHeight,
+	contact.clientHeight,
+];
 
-const secHeights = [home.clientHeight, about.clientHeight, projects.clientHeight, skills.clientHeight, education.clientHeight, contact.clientHeight];
+window.addEventListener('scroll', function () {
 
-// for nav selection animation
-window.addEventListener('scroll', function(){
-	// let sum = window.innerHeight/2;
-	let sum = 2*secHeights[0]/3;
-	let i=1;
-	while(sum < window.scrollY){
-		sum += secHeights[i-1]/3 + 2*secHeights[i]/3;
+	// for nav selection animation
+
+	let sum = (2 * secHeights[0]) / 3;
+	let i = 1;
+	while (sum < window.scrollY) {
+		sum += secHeights[i - 1] / 3 + (2 * secHeights[i]) / 3;
 		i++;
 	}
 	i--;
@@ -39,13 +32,25 @@ window.addEventListener('scroll', function(){
 		navSec[j].classList.remove('selected');
 	}
 	navSec[i].classList.add('selected');
-})
+
+	// section title animations
+	sum = secHeights[0] / 4;
+	i = 1;
+	// when 1/4th of the next section is scrolled into the view then add logo class to it and every section above it also
+	while (sum < window.scrollY) {
+		sum += (3 * secHeights[i - 1]) / 4 + (1 * secHeights[i]) / 4;
+		i++;
+	}
+	for (let j = 0; j < i; j++) {
+		secs[j].querySelector('svg').classList.add('logo');
+	}
+});
 
 // textarea auto resize height code snippet
-textarea = document.querySelector("#autoresizing"); 
-textarea.addEventListener('input', autoResize, false); 
+textarea = document.querySelector('#autoresizing');
+textarea.addEventListener('input', autoResize, false);
 
-function autoResize() { 
-	this.style.height = 'auto'; 
-	this.style.height = this.scrollHeight + 'px'; 
-} 
+function autoResize() {
+	this.style.height = 'auto';
+	this.style.height = this.scrollHeight + 'px';
+}
